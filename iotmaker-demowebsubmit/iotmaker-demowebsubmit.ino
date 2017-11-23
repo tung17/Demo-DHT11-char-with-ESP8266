cee1cdc918d298a2d6a36f1d27fbae350ad6dc41
tungtrinh17 <1414556@hcmut.edu.vn>
2017-11-23 09:27:26 +0700

    nhiet_do_do_am

--- iotmaker-demowebsubmit/iotmaker-demowebsubmit.ino
+++ iotmaker-demowebsubmit/iotmaker-demowebsubmit.ino
@@ -7,6 +7,17 @@
 #include <ArduinoJson.h>
 #include "gpio.h"
 #include <FS.h>   // Include the SPIFFS library
+#include <DHT.h>
+
+#define DHTPIN 5
+#define DHTTYPE DHT11
+
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const char *ssid = "HACKER1";
 
 ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
 
@@ -25,6 +36,14 @@ String change_data(char* json)
   gpio.set(allParams);
   return method;
 }
+bool load_data(float temp,float humd)
+{
+  String temp_char = String(temp,1);
+  String humd_char = String(humd,1);
+  String json1 ="";
+  json1 = String("{") + "\"nhietdo\":" + String(temp)+ ","+ "\"doam\":" +String(humd) + "}";
+  webSocket.broadcastTXT(json1);
+}
 String getContentType(String filename); // convert the file extension to the MIME type
 bool handleFileRead(String path);       // send the right file to the client (if it exists)
 
@@ -41,38 +60,40 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
       Serial.println();
       //Serial.print(Json);
       change_data(Json);
-      webSocket.broadcastTXT("data");
+      //webSocket.broadcastTXT("data");
       break;
   }
 
 }
 
 void setup() {
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  Serial.begin(115200);        // Start the Serial communication to send messages to the computer
+  WiFi.softAP(ssid);
   delay(10);
+  dht.begin();
   Serial.println('\n');
   pinMode(16, OUTPUT);
-  wifiMulti.addAP("420super", "024024024");   // add Wi-Fi networks you want to connect to
+  /*wifiMulti.addAP("420super", "024024024");   // add Wi-Fi networks you want to connect to
   wifiMulti.addAP("iotmaker_nho", "@iotmaker.vn");
-  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");*/
 
   Serial.println("Connecting ...");
   int i = 0;
-  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  /*while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
     delay(250);
     Serial.print('.');
-  }
-  Serial.println('\n');
+  }*/
+  /*Serial.println('\n');
   Serial.print("Connected to ");
   Serial.println(WiFi.SSID());              // Tell us what network we're connected to
   Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+  Serial.println(WiFi.localIP());  */         // Send the IP address of the ESP8266 to the computer
 
-  if (MDNS.begin("http://esp8266",WiFi.localIP())) {              // Start the mDNS responder for esp8266.local
+  /*if (MDNS.begin("http://esp8266",WiFi.localIP())) {              // Start the mDNS responder for esp8266.local
     Serial.println("mDNS responder started");
   } else {
     Serial.println("Error setting up MDNS responder!");
-  }
+  }*/
 
   SPIFFS.begin();                           // Start the SPI Flash Files System
   
@@ -88,8 +109,15 @@ void setup() {
 }
 
 void loop(void) {
+  unsigned long currentMillis = millis();
   server.handleClient();
   webSocket.loop();
+  float temp = dht.readTemperature();
+  float humid = dht.readHumidity();
+   if(currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+  load_data(temp,humid);
+   }
 }
 
 String getContentType(String filename) { // convert the file extension to the MIME type
